e660088daf48845d54792c2b11a546328a4d4c74
Muzaffar Ahmed <muzaffarahmed75@gmail.com>
2020-06-20 12:49:10 +0530

    Fix graph plotter commented lines issue
    Graph plotter now skips commented lines in the data.
    Changed tabs to whitespace.

--- graph_plotter.py
+++ graph_plotter.py
@@ -15,12 +15,15 @@ options = {
 G = nx.DiGraph()
 graph_file = input("filename: ")
 with open("data/" + graph_file, "r") as content_file:
-	while True:
-		line = content_file.readline()
-		if line == "": break
-		u, v = line.split()
-		G.add_edge(u, v)
-	content_file.close()
+    while True:
+        line = content_file.readline()
+        if not line:
+            break
+        if line[0] == '#':
+            continue
+        u, v = line.split()
+        G.add_edge(u, v)
+    content_file.close()
 
 shape = nx.spring_layout(G, seed=10)
 nx.draw(G, pos=shape, **options)
@@ -31,24 +34,18 @@ G_scc = nx.DiGraph()
 colors = np.array([])
 stem = splitext(graph_file)[0]
 with open("output/" + stem + ".dcsc.out.txt", "r") as content_file:
-	# read first line as dummy
-	content_file.readline()
-	while True:
-		line = content_file.readline()
-		if line == "\n": break
-		sz, vertices = line.split(": ")
-		vertices = [node for node in vertices.strip().split()]
-		subgraph = G.subgraph(vertices)
-		colors = np.append(colors, np.ones((int(sz), )) * np.random.randint(255))
-		G_scc = nx.union(G_scc, subgraph)
-	content_file.close()
+    # read first line as dummy
+    content_file.readline()
+    while True:
+        line = content_file.readline()
+        if line == "\n": break
+        sz, vertices = line.split(": ")
+        vertices = [node for node in vertices.strip().split()]
+        subgraph = G.subgraph(vertices)
+        colors = np.append(colors, np.ones((int(sz), )) * np.random.randint(255))
+        G_scc = nx.union(G_scc, subgraph)
+    content_file.close()
 
 shape = nx.spring_layout(G_scc, seed=10)
 nx.draw(G_scc, pos=shape, cmap="jet", node_color=colors, **options)
 plt.show()
-
-
-
-
-
-
